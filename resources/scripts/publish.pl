d9066e53a19580b51e265f95472eecb85ba24c63
Kleyson de Sousa Rios <kleyson.rios@saude.go.gov.br>
2015-08-25 09:36:52 -0300

    - Adicionado script publish.pl - Ajustado defaults dentro do sync.pl

--- resources/scripts/publish.pl
+++ resources/scripts/publish.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-# Script to synchronize
+# Script to publish mondrian schema
 # Author: Kleyson Rios
 
 use DBI;
@@ -14,6 +14,9 @@ my $pentaho_server;
 my $pentaho_port;
 my $solution;
 my $path;
+my $mondrianSchema;
+my $datasourceName;
+my $xmlaFlag;
 
 options();
 
@@ -63,7 +66,7 @@ print "Token inserted successfully\n";
 # Call /sync API
 #
 ######################################
-my $api = "http://$pentaho_server:$pentaho_port/pentaho/plugin/fastsync/api/sync --data-urlencode 'type=token' --data-urlencode 'token=$token' --data-urlencode 'solution=$solution' --data-urlencode 'delete=true' --data-urlencode 'deletePerm=true' --data-urlencode 'path=$path' --data-urlencode 'urlEncoded=$urlEncoded'";
+my $api = "http://$pentaho_server:$pentaho_port/pentaho/plugin/fastsync/api/publish --data-urlencode 'type=token' --data-urlencode 'token=$token' --data-urlencode 'solution=$solution' --data-urlencode 'path=$path' --data-urlencode 'schema=$mondrianSchema' --data-urlencode 'datasourceName=$datasourceName' --data-urlencode 'xmlaEnabledFlag=$xmlaFlag' --data-urlencode 'urlEncoded=$urlEncoded'";
 
 my $apiReturn = `/usr/bin/curl -s -G $api`;
 
@@ -79,10 +82,13 @@ sub options() {
                'host=s' => \$pentaho_server,
                'port=s' => \$pentaho_port,
                'solution=s' => \$solution,
-               'path=s' => \$path) or die "Usage: $0 --user PENTAHO_USER --host PENTAHO_SERVER --port PORT --solution SOLUTION --path PATH\n";
+               'path=s' => \$path,
+               'schema=s' => \$mondrianSchema,
+               'datasource=s' => \$datasourceName,
+               'xmlaFlag=s' => \$xmlaFlag) or die "Usage: $0 --user PENTAHO_USER --host PENTAHO_SERVER --port PORT --solution SOLUTION --path PATH --schema MONDRIAN_SCHEMA --datasource JNDI_NAME --xmlaFlag TRUE/FALSE\n";
 
-    if (! $solution || ! $path || ! $user || ! $pentaho_server || ! $pentaho_port) {
-         die "Usage: $0 --user PENTAHO_USER --host PENTAHO_SERVER --port PORT --solution SOLUTION --path PATH\n";
+    if (! $solution || ! $path || ! $user || ! $pentaho_server || ! $pentaho_port || ! $mondrianSchema || ! $datasourceName || ! $xmlaFlag) {
+         die "Usage: $0 --user PENTAHO_USER --host PENTAHO_SERVER --port PORT --solution SOLUTION --path PATH --schema MONDRIAN_SCHEMA --datasource JNDI_NAME --xmlaFlag TRUE/FALSE\n";
     }
 
 }
